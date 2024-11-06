ec28cc61216b3a94a5ae0aba7d758a299129b9f2
Joakim Vu <joakimvu@icloud.com>
2024-11-06 21:26:21 +0100

    Create NavBar.jsx

---
+++ ecommerce/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "next/link";
+
+import { AiOutlineShopping } from "react-icons/ai";
+
+const NavBar = () => {
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">JV Headphones</Link>
+      </p>
+
+      <button type="button" className="cart-icon">
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
+  );
+};
+
+export default NavBar;
